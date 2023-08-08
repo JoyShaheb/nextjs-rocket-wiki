8170e7c195dd75d3f7dea219f839616709b2f6af
joyshaheb <khondokoralam@gmail.com>
2023-08-08 12:23:48 +0600

    carousel work in progress

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,145 +1,82 @@
-import React, { FC } from "react";
+"use client";
+import React, { FC, useState } from "react";
 import Image from "next/image";
-import { nanoid } from "nanoid";
-import { ChevronLeftIcon } from "@heroicons/react/24/outline";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 
 interface ICarouselProps {
   images: string[];
 }
 
 const Carousel: FC<ICarouselProps> = ({ images }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSlideChange = (index: number) => setActiveIndex(index);
+
   return (
     <div
       id="indicators-carousel"
       className="relative w-full"
       data-carousel="static"
     >
-      {/* <!-- Carousel wrapper --> */}
       <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-        {/* <!-- Item 1 --> */}
-        {images?.map((image, index) => (
+        {images.map((image, index) => (
           <div
-            className="hidden duration-700 ease-in-out"
+            className={`${
+              activeIndex === index ? "block" : "hidden"
+            } duration-700 ease-in-out`}
             data-carousel-item="active"
-            key={nanoid()}
+            key={index}
           >
             <Image
-              src="/docs/images/carousel/carousel-1.svg"
+              src={image}
               className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
+              alt={`Slide ${index + 1}`}
               width={200}
               height={200}
             />
           </div>
         ))}
-
-        {/* <!-- Item 2 --> */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <Image
-            src="/docs/images/carousel/carousel-1.svg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-            width={200}
-            height={200}
-          />
-        </div>
-        {/* <!-- Item 3 --> */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <Image
-            src="/docs/images/carousel/carousel-1.svg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-            width={200}
-            height={200}
-          />
-        </div>
       </div>
-      {/* <!-- Slider indicators --> */}
       <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="true"
-          aria-label="Slide 1"
-          data-carousel-slide-to="0"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 2"
-          data-carousel-slide-to="1"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 3"
-          data-carousel-slide-to="2"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 4"
-          data-carousel-slide-to="3"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 5"
-          data-carousel-slide-to="4"
-        ></button>
+        {images.map((_, index) => (
+          <button
+            type="button"
+            className={`w-3 h-3 rounded-full ${
+              activeIndex === index ? "bg-white" : ""
+            }`}
+            aria-current={activeIndex === index}
+            aria-label={`Slide ${index + 1}`}
+            data-carousel-slide-to={index}
+            onClick={() => handleSlideChange(index)}
+            key={index}
+          ></button>
+        ))}
       </div>
-      {/* <!-- Slider controls --> */}
       <button
         type="button"
         className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         data-carousel-prev
+        onClick={() =>
+          handleSlideChange((activeIndex - 1 + images.length) % images.length)
+        }
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-        </span>
-        <span className="sr-only">Previous</span>
+        <ChevronLeftIcon
+          style={{
+            border: "2px solid red",
+            backgroundColor: "red",
+          }}
+          className="w-4 h-4 text-white dark:text-gray-800"
+          strokeWidth={2}
+        />
       </button>
       <button
         type="button"
         className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         data-carousel-next
+        onClick={() => handleSlideChange((activeIndex + 1) % images.length)}
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
+        {/* Icon and label */}
+        <ChevronRightIcon className="w-4 h-4 text-white dark:text-gray-800" />
       </button>
     </div>
   );
